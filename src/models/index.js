11593d7dc2584366e288e7181488395242c7cbe1
Kaweesha1994 <kaweeshahasarinda@gmail.com>
2022-03-18 21:19:19 +0530

    changed file name to capital

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const {UserSchema} = require('./User');
 const {RoleSchema} = require('./Role');
 const {VehicleSchema} = require('./Vehicle');
-const {VehicleImageSchema} = require("./vehicleImage");
+const {VehicleImageSchema} = require("./VehicleImage");
 
 mongoose.connect('mongodb://localhost:27017/buildex').then( (res) => {
  console.log('DB Connected');
