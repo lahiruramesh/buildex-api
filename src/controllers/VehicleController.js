2b2ef828155354366a2b778076175482ce53ccac
Kaweesha1994 <kaweeshahasarinda@gmail.com>
2022-03-18 21:23:50 +0530

    vehicle update method

--- src/controllers/VehicleController.js
+++ src/controllers/VehicleController.js
@@ -40,3 +40,25 @@ exports.findOne = (req, res) => {
         .send({ message: "Error retrieving Vehicle with id=" + id });
     });
 };
+
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+  const id = req.params.id;
+  VehicleModel.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+        });
+      } else res.send({ message: "Tutorial was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id
+      });
+    });
+};
--- src/routers/Vehicle.js
+++ src/routers/Vehicle.js
@@ -5,6 +5,7 @@ const vehicleController = require('../controllers/VehicleController');
 
 vehicleRouter.post('/create', vehicleController.create);
 vehicleRouter.get("/:id", vehicleController.findOne);
+vehicleRouter.put("/:id", vehicleController.update);
 
 module.exports = {
   vehicleRouter
