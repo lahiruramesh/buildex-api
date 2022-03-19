12999531aec24426175303aa08855ea99ca40ac7
prabodha <prabolenora@gmail.com>
2022-03-19 10:51:35 +0530

    resolve conflicts

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,23 +1,19 @@
 const {authRouter} = require('./auth');
 const {vehicleRouter} = require("./Vehicle");
 const {vehicleImageRouter} = require("./VehicleImage");
-<<<<<<< HEAD
+
 const {vehicleServiceRouter} = require("./VehicleService");
 const {feedbackRouter} = require("./Feedback");
-=======
+
 const {timetableRouter} = require("./Timetable");
 
->>>>>>> 390a26f9039f5d42e98e97971d2776a01442cffc
+
 
 module.exports = {
     authRouter,
     vehicleRouter,
     vehicleImageRouter,
-<<<<<<< HEAD
     vehicleServiceRouter,
-    feedbackRouter
-=======
+    feedbackRouter,
     timetableRouter,
-
->>>>>>> 390a26f9039f5d42e98e97971d2776a01442cffc
 };
